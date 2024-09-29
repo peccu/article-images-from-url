20b076d4c6149c20c09a5ae96b5550c1f9c209a6
peccu <peccul+github@gmail.com>
2024-09-29 01:00:18 +0000

    add CORS headers

--- netlify/functions/feedExtractor.ts
+++ netlify/functions/feedExtractor.ts
@@ -25,6 +25,11 @@ const handler: Handler = async (event) => {
 
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Methods": "*",
+      },
       body: JSON.stringify({ urls }),
     }
   } catch (error) {
